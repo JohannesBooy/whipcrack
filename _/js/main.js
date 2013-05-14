a7b1aef6c5fd3de856eb4463be5ad48ff78c1371
Dan Perrera <dan@fromtheoutfit.com>
2013-05-14 16:53:05 -0400

    This is where we're starting this repo from.

---
+++ _/js/main.js
@@ -0,0 +1,1 @@
+// @codekit-prepend "vendor/modernizr-2.6.2.min.js", "vendor/jquery-1.9.1.min.js", "plugins.js"
---
+++ config.rb
@@ -0,0 +1,22 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "_/css"
+sass_dir = "_/sass"
+images_dir = "_/img"
+javascripts_dir = "_/js"
+
+output_style = :expanded
+environment = :development
+
+relative_assets = true
+line_comments = true
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass _/sass scss && rm -rf sass && mv scss sass
