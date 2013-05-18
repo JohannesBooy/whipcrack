4db1ae16740c6d4208b6b9c1625b57c324ec0f92
Marc Amos <marc@fromtheoutfit.com>
2013-05-18 19:46:44 -0400

    A rough first-pass at creating a demo-document.

--- index.php
+++ index.php
@@ -17,7 +17,84 @@
 			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 		<![endif]-->
 
-		<!-- Add your site or application content here -->
+
+		<div class="gw">
+			<div class="c">
+				<div class="g one-whole">
+					<h1>This is an &lt;h1&gt;</h1>
+					<p>This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph.</p>
+
+					<h2>This is an &lt;h2&gt;</h2>
+					<ol>
+						<li>Ordered list item 1</li>
+						<li>Ordered list item 2</li>
+						<li>Ordered list item 3</li>
+						<li>Ordered list item 4</li>
+						<li>Ordered list item 5</li>
+						<li>Ordered list item 6</li>
+						<li>Ordered list item 7</li>
+						<li>Ordered list item 8</li>
+						<li>Ordered list item 9</li>
+						<li>Ordered list item 10</li>
+						<li>Ordered list item 11</li>
+						<li>Ordered list item 12</li>
+					</ol>
+
+					<h3>This is an &lt;h3&gt;</h3>
+					<ul>
+						<li>Unordered list item 1</li>
+						<li>Unordered list item 2</li>
+						<li>Unordered list item 3</li>
+						<li>Unordered list item 4</li>
+						<li>Unordered list item 5</li>
+						<li>Unordered list item 6</li>
+					</ul>
+
+					<h4>This is an &lt;h4&gt;</h4>
+					<dl>
+						<dt>Description list name 1</dt>
+						<dd>Description list value 1</dd>
+
+						<dt>Description list name 2</dt>
+						<dd>Description list value 2a</dd>
+						<dd>Description list value 2b</dd>
+						<dd>Description list value 2c</dd>
+
+						<dt>Description list name 3a</dt>
+						<dt>Description list name 3b</dt>
+						<dd>Description list value 3</dd>
+					</dl>
+
+					<h5>This is an &lt;h5&gt;</h5>
+					<p>This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph.</p>
+				</div><!-- g one-whole -->
+			</div><!-- c -->
+		</div><!-- gw -->
+
+
+		<hr />
+
+
+		<div class="gw">
+			<div class="c">
+				<div class="g one-whole s-one-half m-one-third l-one-quarter">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div><!-- g one-whole s-one-half m-one-third l-one-quarter -->
+
+				<div class="g one-whole s-one-half m-one-third l-one-quarter">
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div><!-- g one-whole s-one-half m-one-third l-one-quarter -->
+
+				<div class="g one-whole s-one-half m-one-third l-one-quarter">
+					<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div><!-- g one-whole s-one-half m-one-third l-one-quarter -->
+
+				<div class="g one-whole s-one-half m-one-third l-one-quarter">
+					<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div><!-- g one-whole s-one-half m-one-third l-one-quarter -->
+			</div><!-- .c -->
+		</div><!-- .gw -->
+
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="/_/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
