849cf00b33775aabe1c008c0eb004b4618a520a3
Marc Amos <marc@fromtheoutfit.com>
2013-05-18 21:23:34 -0400

    Added quite a bit more to the demo-document.

--- index.php
+++ index.php
@@ -20,11 +20,91 @@
 
 		<div class="gw">
 			<div class="c">
-				<div class="g one-whole">
-					<h1>This is an &lt;h1&gt;</h1>
-					<p>This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph.</p>
 
+				<div class="g one-whole">
+					<h1>Grid Demonstrations:</h1>
+				</div>
+				<div class="g one-whole">
+					<h2>One Whole &rarr; One Half &rarr; One Quarter:</h2>
+				</div>
+				<div class="g one-whole s-one-half l-one-quarter">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div><!-- g one-whole s-one-half l-one-quarter -->
+				<div class="g one-whole s-one-half l-one-quarter">
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div><!-- g one-whole s-one-half l-one-quarter -->
+				<div class="g one-whole s-one-half l-one-quarter">
+					<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div><!-- g one-whole s-one-half l-one-quarter -->
+				<div class="g one-whole s-one-half l-one-quarter">
+					<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div><!-- g one-whole s-one-half l-one-quarter -->
+
+				<div class="g one-whole">
+					<h2>One Whole &rarr; One Third:</h2>
+				</div>
+				<div class="g one-whole m-one-third">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div><!-- g one-whole m-one-third -->
+				<div class="g one-whole m-one-third">
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div><!-- g one-whole m-one-third -->
+				<div class="g one-whole m-one-third">
+					<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div><!-- g one-whole m-one-third -->
+
+				<div class="g one-whole">
+					<h2>One Whole &rarr; One Fifth &amp; Four Fifths:</h2>
+				</div>
+				<div class="g one-whole m-one-fifth">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div><!-- g one-whole m-one-fifth -->
+				<div class="g one-whole m-four-fifths">
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div><!-- g one-whole m-four-fifths -->
+
+				<div class="g one-whole">
+					<h2>One Whole &rarr; One Fifth:</h2>
+				</div>
+				<div class="g one-whole m-one-fifth">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div><!-- g one-whole m-one-fifth -->
+				<div class="g one-whole m-one-fifth">
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div><!-- g one-whole m-one-fifth -->
+				<div class="g one-whole m-one-fifth">
+					<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div><!-- g one-whole m-one-fifth -->
+				<div class="g one-whole m-one-fifth">
+					<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div><!-- g one-whole m-one-fifth -->
+				<div class="g one-whole m-one-fifth">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div><!-- g one-whole m-one-fifth -->
+
+			</div><!-- .c -->
+		</div><!-- .gw -->
+
+		<hr />
+
+		<div class="gw">
+			<div class="c">
+				<div class="g one-whole">
+
+					<h1>Inline Elements:</h1>
+					<p>This is a paragraph that attempts to render all of the common inline elements. Here is <a href="http://a-url-that-is-not-in-your-browser-history.com/">anchor</a>. Here is <b>bold</b>. Here is <strong>strong</strong>. Here is <i>italic</i>. Here is <em>emphasis</em>.  Here is <small>small</small>. Here is <kbd>keyboard input</kbd>. Here is <code>code</code>. Here is <q>quote</q>. Here is <cite>cite</cite>. Here is <dfn>definition</dfn>. Here is <sup>superscript</sup>. Here is <sub>subscript</sub>. Here is <mark>mark</mark>. Here is <del>delete</del>. Here is <ins>insert</ins>. Here is <abbr>abbreviation</abbr>.</p>
+
+					<hr />
+
+					<h1>Block-level Elements:</h1>
+					<h1>This is an &lt;h1&gt;</h1>
 					<h2>This is an &lt;h2&gt;</h2>
+					<h3>This is an &lt;h3&gt;</h3>
+					<h4>This is an &lt;h4&gt;</h4>
+					<h5>This is an &lt;h5&gt;</h5>
+					<blockquote>Here is blockquote.</blockquote>
+					<address>Here is address.</address>
+					<pre>Here is preformatted text.</pre>
 					<ol>
 						<li>Ordered list item 1</li>
 						<li>Ordered list item 2</li>
@@ -39,8 +119,6 @@
 						<li>Ordered list item 11</li>
 						<li>Ordered list item 12</li>
 					</ol>
-
-					<h3>This is an &lt;h3&gt;</h3>
 					<ul>
 						<li>Unordered list item 1</li>
 						<li>Unordered list item 2</li>
@@ -49,8 +127,6 @@
 						<li>Unordered list item 5</li>
 						<li>Unordered list item 6</li>
 					</ul>
-
-					<h4>This is an &lt;h4&gt;</h4>
 					<dl>
 						<dt>Description list name 1</dt>
 						<dd>Description list value 1</dd>
@@ -65,35 +141,75 @@
 						<dd>Description list value 3</dd>
 					</dl>
 
-					<h5>This is an &lt;h5&gt;</h5>
-					<p>This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph.</p>
+					<hr />
+
+					<h1>Media-like Elements:</h1>
+					<p>An img element: <img src="http://fromtheoutfit.com/_/img/logo.png" alt="The Outfit logo" /></p>
+					<p>
+						A figure element, containing img and figcaption elements:
+						<figure>
+							<img src="http://fromtheoutfit.com/_/img/logo.png" alt="The Outfit logo" />
+							<figcaption>A nice logo from a nice company.</figcaption>
+						</figure>
+					</p>
+					<table>
+						<caption>Table caption</caption>
+						<thead>
+							<tr>
+								<th>Header 1</th>
+								<th>Header 2</th>
+								<th>Header 3</th>
+								<th>Header 4</th>
+							</tr>
+						</thead>
+						<tfoot>
+							<tr>
+								<td>Footer 1</td>
+								<td>Footer 2</td>
+								<td>Footer 3</td>
+								<td>Footer 4</td>
+							</tr>
+						</tfoot>
+						<tbody>
+							<tr>
+								<td>Body content A1</td>
+								<td>Body content A2</td>
+								<td>Body content A3</td>
+								<td>Body content A4</td>
+							</tr>
+							<tr>
+								<td>Body content B1</td>
+								<td>Body content B2</td>
+								<td>Body content B3</td>
+								<td>Body content B4</td>
+							</tr>
+							<tr>
+								<td>Body content C1</td>
+								<td>Body content C2</td>
+								<td>Body content C3</td>
+								<td>Body content C4</td>
+							</tr>
+						</tbody>
+					</table>
+					<p>
+						A video element:<br />
+						<video controls>
+							<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm" />
+							<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg" />
+							<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4" />
+							<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp" />
+						</video>
+					</p>
+					<p>
+						An audio element:<br />
+						<audio controls>
+							<source src="http://www.lunerouge.org/sons/animaux/LRStorm%2001%20by%20Teza.ogg" type="audio/ogg" />
+						</audio>
+					</p>
+
 				</div><!-- g one-whole -->
 			</div><!-- c -->
 		</div><!-- gw -->
-
-
-		<hr />
-
-
-		<div class="gw">
-			<div class="c">
-				<div class="g one-whole s-one-half m-one-third l-one-quarter">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				</div><!-- g one-whole s-one-half m-one-third l-one-quarter -->
-
-				<div class="g one-whole s-one-half m-one-third l-one-quarter">
-					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				</div><!-- g one-whole s-one-half m-one-third l-one-quarter -->
-
-				<div class="g one-whole s-one-half m-one-third l-one-quarter">
-					<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-				</div><!-- g one-whole s-one-half m-one-third l-one-quarter -->
-
-				<div class="g one-whole s-one-half m-one-third l-one-quarter">
-					<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</div><!-- g one-whole s-one-half m-one-third l-one-quarter -->
-			</div><!-- .c -->
-		</div><!-- .gw -->
 
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
